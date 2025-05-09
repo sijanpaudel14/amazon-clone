27a0fdf9c44381c1387e5fac9b06dc710a3fda98
Sijan Paudel <sijanpaudel44@gmail.com>
2025-05-10 03:17:57 +0545

    create a website layout

---
+++ lib/constant.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more!'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'NxtAmzn is a Next.js 13.4+ e-commerce template with Tailwind CSS, TypeScript. It is designed to be a starting point for building your own e-commerce application.'
---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deals",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-sellers',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrivals',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured-products',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+  ],
+}
+export default data
